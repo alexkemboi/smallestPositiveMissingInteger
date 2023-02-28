9b33d04c4cb28cb15d94763f211c2443b43b0a52
alexkemboi4127 <alexkemboi97@yahoo.com>
2023-02-28 16:02:21 +0300

    fixed scenario when no missing value,when values less than zero

--- smallestPositiveMissingInteger.js
+++ smallestPositiveMissingInteger.js
@@ -1,28 +1,43 @@
-let A = [1, 3, 2, 1, 4, 5, 0, 8];
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+
 function solution(A) {
+  // Implement your solution here
   let largestValue = 1;
   let smallestPositiveMissingInteger = 1;
   let missingIntegers = [];
+  let countMissingIntegers = 0;
+
+  //finds the largest value in the array
   for (i = 0; i < A.length; i++) {
     if (largestValue < A[i]) {
       largestValue = A[i];
     }
   }
 
-  for (x = 0; x < largestValue; x++) {
-    if (A.includes(x)) {
-      // console.log("not missing");
+  if (largestValue > 0) {
+    //finds missing value and adds to new array
+    for (x = 1; x < largestValue + 1; x++) {
+      if (!A.includes(x)) {
+        missingIntegers.push(x);
+        countMissingIntegers = countMissingIntegers + 1;
+      }
+    }
+    if (countMissingIntegers > 0) {
+      smallestPositiveMissingInteger = missingIntegers[0];
+      //finds the smallest value from the new array with missing values
+      for (b = 0; b < missingIntegers.length; b++) {
+        if (smallestPositiveMissingInteger >= missingIntegers[b]) {
+          smallestPositiveMissingInteger = missingIntegers[b];
+        }
+      }
     } else {
-      missingIntegers.push(x);
+      smallestPositiveMissingInteger = A[A.length - 1] + 1;
     }
+  } else {
+    smallestPositiveMissingInteger = 1;
   }
-  for (b = 0; b < missingIntegers.length; b++) {
-    smallestPositiveMissingInteger = missingIntegers[0];
-    if (smallestPositiveMissingInteger >= missingIntegers[b]) {
-      smallestPositiveMissingInteger = missingIntegers[b];
-    }
-  }
-
-  console.log(smallestPositiveMissingInteger);
+  return smallestPositiveMissingInteger;
 }
-solution(A);
+let A = [1,2,3];
+console.log(solution(A));
